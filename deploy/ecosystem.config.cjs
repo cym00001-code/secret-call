1e65ce3085898cad46f9a071473335f12d5a398d
晨 <nipakrana@gmail.com>
2026-05-17 19:35:08 +0800

    chore: configure v2 room ttl for pm2

--- deploy/ecosystem.config.cjs
+++ deploy/ecosystem.config.cjs
@@ -8,7 +8,12 @@ module.exports = {
       env: {
         NODE_ENV: "production",
         PORT: "3101",
-        HOST: "127.0.0.1"
+        HOST: "127.0.0.1",
+        ROOM_TTL_MS: "86400000",
+        ROOM_SUSPENDED_TTL_MS: "7200000",
+        MESSAGE_TTL_MS: "7200000",
+        BURNED_ID_TTL_MS: "7200000",
+        CLIENT_TIMEOUT_MS: "35000"
       }
     },
     {
